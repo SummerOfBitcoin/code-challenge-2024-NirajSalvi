4f594989d6453d27f4f7a8f7a4444c1d8fc63900
Niraj Salvi <nnsalvi2014@gmail.com>
2024-04-28 14:20:27 +0530

    298tx

--- src/main.rs
+++ src/main.rs
@@ -15,9 +15,9 @@ fn main() {
     // let paths = fs::read_dir("mempool/").unwrap();
     // let paths = fs::read_dir("mempool_2/").unwrap();
     // let paths = fs::read_dir("mempool_3/").unwrap();
-    // let paths = fs::read_dir("mempool_4/").unwrap();
+    let paths = fs::read_dir("mempool_4/").unwrap();
     // let paths = fs::read_dir("mempool_5/").unwrap();
-    let paths = fs::read_dir("mempool_6/").unwrap();
+    // let paths = fs::read_dir("mempool_6/").unwrap();
 
     // let paths = fs::read_dir("serialize/").unwrap();
     // let paths = fs::read_dir("p2pkh/").unwrap();

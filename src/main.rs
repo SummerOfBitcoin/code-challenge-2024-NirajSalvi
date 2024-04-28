749b65141f525187b33a857aa16216af8f08848f
Niraj Salvi <nnsalvi2014@gmail.com>
2024-04-28 11:03:29 +0530

    Error resolved ?

--- src/main.rs
+++ src/main.rs
@@ -12,11 +12,11 @@ use crate::tx::dsha256;
 fn main() {
     let mut tx_vec: Vec<tx::btctx> = Vec::new();
 
-    // let paths = fs::read_dir("mempool/").unwrap();
+    let paths = fs::read_dir("mempool/").unwrap();
     // let paths = fs::read_dir("serialize/").unwrap();
     // let paths = fs::read_dir("p2pkh/").unwrap();
     // let paths = fs::read_dir("multi_input_p2pkh/").unwrap();
-    let paths = fs::read_dir("multi_input_p2wpkh/").unwrap();
+    // let paths = fs::read_dir("multi_input_p2wpkh/").unwrap();
 
     for path in paths {
         // println!("Name: {}", path.unwrap().path().display());
